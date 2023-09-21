94786e92cfe073e746e1c7b47a4f9fe32d8588d3
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-09-21 15:39:37 +0200

    initiate commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+
+var number = 1;
+
+while (number != 150)
+{
+    if (number % 105 == 0)
+    {
+        Console.WriteLine("Fizzbuzzbazz");
+    }
+    else if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("Fizzbuzz");
+    }
+    else if (number % 3 == 0 && number % 7 == 0)
+    {
+        Console.WriteLine("Fizzbazz");
+    }
+    else if (number % 5 == 0 && number % 7 == 0)
+    {
+        Console.WriteLine("Buzzbazz");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (number % 7 == 0)
+    {
+        Console.WriteLine("Bazz");
+    }
+    else { Console.WriteLine(number); }
+    number = number + 1;
+}
+
